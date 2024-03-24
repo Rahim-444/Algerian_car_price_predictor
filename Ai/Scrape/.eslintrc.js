cca1627d941bfbdca13d2943daa51ddf2c42b4c0
Rahim-444 <abderrahimbelkacemi33@gmail.com>
2024-03-24 23:24:39 +0100

    fetched data will be saved in the database

--- Ai/Scrape/.eslintrc.js
+++ Ai/Scrape/.eslintrc.js
@@ -1,23 +1,27 @@
 module.exports = {
-    env: {
-        browser: true,
-        commonjs: true,
-        es2021: true,
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true,
+        "node": true
     },
-    extends: "eslint:recommended",
-    overrides: [
+    "extends": "eslint:recommended",
+    "overrides": [
         {
-            env: {
-                node: true,
+            "env": {
+                "node": true
             },
-            files: [".eslintrc.{js,cjs}"],
-            parserOptions: {
-                sourceType: "script",
-            },
-        },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
     ],
-    parserOptions: {
-        ecmaVersion: "latest",
+    "parserOptions": {
+        "ecmaVersion": "latest"
     },
-    rules: {},
-};
+    "rules": {
+    }
+}
